9731c7966b7912acc208c17a304c0cf0b6c0883e
guideau jean <guideau221@unicaen.fr>
2023-10-16 14:56:05 +0200

    add header sur toutes les pages actuel et debut nav bar;

--- PHP/header.php
+++ PHP/header.php
@@ -1,12 +1,17 @@
 <?php
 echo '<header>';
 
-echo ' <ul>
-        <li><a href="default.asp">Home</a></li>
-        <li><a href="news.asp">News</a></li>
-        <li><a href="contact.asp">Contact</a></li>
-        <li><a href="about.asp">About</a></li>
-       </ul> ';
+echo '   <nav>
+            <div class="nav-wrapper">
+                <img src="../image-logo/Logo.png"/>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="nosRecettes.php">Nos recettes</a></li>
+                    <li><a href="filtres.php">Filtres</a></li>
+                    <li><a href="connexion.php">Connexion</a></li>
+                </ul>
+            </div>
+         </nav>';
 
 echo '</header>';
 ?>

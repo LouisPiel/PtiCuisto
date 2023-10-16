cbfec18263d0b8affb95be0e52f629192ff4363f
Sirmetana <lukas.siopathis@gmail.com>
2023-10-16 16:00:29 +0200

    Début Formatage filtres

--- index.php
+++ index.php
@@ -32,7 +32,31 @@
             .$row["id"]." ".$row["Categorie"]."</p></div>";
         }
         } else {
-        echo "Aucun résultat";
+            echo "Aucun résultat";
+        }
+
+        //Récupérer les recettes selon filtre
+        $filtreNom = $_POST["selectFiltreNom"];
+        $filtreNom = $_POST["selectFiltreCategorie"];
+        $filtreNom = $_POST["selectFiltreIngredient"];
+        $filtreNom = $_POST["selectFiltreNom"];
+        if ($filtreNom != NULL)
+        {
+            $sql = "SELECT * FROM Recette where Titre like \"%".$filtreNom."\"";
+            $result = $conn->query($sql);
+            
+            if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "<div><h4>". $row["Titre"]."</h4><br><p class=\"resume\">".$row["Résumé"].
+                "</p><br><p class=\"contenu\">".$row["Contenu"]."</p><br><p class=\"categorie\">"
+                .$row["Categorie"]."</p><br><p class=\"image\">".$row["Image"]."</p><br><p class=\"id auteur\">"
+                .$row["Identifiant"]." ".$row["Auteur"]."</p>
+                </div>";
+            }
+            } else {
+                echo "Aucun résultat";
+            }
         }
 
 

50799bb81367f3647feff96c7d90fe39f553fd4d
Sirmetana <lukas.siopathis@gmail.com>
2023-10-23 09:57:29 +0200

    Deuxième jet

--- Scripts/nos_recettes.js
+++ Scripts/nos_recettes.js
@@ -1,3 +1,8 @@
+var modaleNom = document.getElementById("modaleNom");
+var modaleCategorie = document.getElementById("modaleCategorie");
+var modaleIngredient = document.getElementById("modaleIngredient");
+
+
 var addNom = document.getElementById("button_ajouter_nom");
 var addIngr = document.getElementById("button_ajouter_ingredient");
 
@@ -6,6 +11,28 @@ var filtreCategorie = document.getElementById("filtre_categorie");
 var filtreIngredient = document.getElementById("filtre_ingredient");
 
 
-addNom.addEventListener('click', function() {
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+addNom.addEventListener('click', function () {
     let inputNom = filtreNom.textContent;
 });
--- nos_recettes.php
+++ nos_recettes.php
@@ -5,36 +5,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos recettes</title>
     <script src="../Scripts/nos_recettes.js"></script>
+    <style>
+        .modal {
+        display: none; /* Hidden by default */
+        position: fixed; /* Stay in place */
+        z-index: 1; /* Sit on top */
+        padding-top: 100px; /* Location of the box */
+        left: 0;
+        top: 0;
+        width: 100%; /* Full width */
+        height: 100%; /* Full height */
+        overflow: auto; /* Enable scroll if needed */
+        background-color: rgb(0,0,0); /* Fallback color */
+        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+    </style>
 </head>
 <body>
     <!--Il faut que chaque filtre ajoute un tag qui limite la recherche-->
+    <!--Fenêtres modales-->
+    <div id="modaleNom" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
+        <span class="close">&times;</span>
+        <h1 id="modal-heading">Filtre nom</h1>
+        <input name="filtre_nom" id="filtre_nom" class="filtre"></input>
+        <button id="button_ajouter_nom">Ajouter</button>            
+    </div>
+    <div id="modaleCategorie" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
+        <span class="close">&times;</span>
+        <h1 id="modal-heading">Filtre ingredient</h1>
+        <select name="filtre_ingredient" id="filtre_ingredient" class="filtre">
+            <option value="">---Choisir un ingrédient---</option>
+            <option value="Entrée">Entrée</option>
+            <option value="Plat principal">Plat principal</option>
+            <option value="Dessert">Dessert</option>
+            <option value="Salade">Salade</option>
+        </select>        
+    </div>
+    <div id="modaleIngredient" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
+        <span class="close">&times;</span>
+        <h1 id="modal-heading">Filtre nom</h1>
+        <input name="filtre_ingredient" id="filtre_ingredient" class="filtre"></input>
+        <button id="button_ajouter_ingredient">Ajouter</button>   
+    </div>
     <section id="bandeau_filtres">
         <p>Filtres</p>
-        <div role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-            <button>Fermer</button>
-            <h1 id="modal-heading">Filtre nom</h1>
-            <input name="filtre_nom" id="filtre_nom" class="filtre"></input>
-            <button id="button_ajouter_nom">Ajouter</button>            
-        </div>
-        <div role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-            <button>Fermer</button>
-            <h1 id="modal-heading">Filtre ingredient</h1>
-            <select name="filtre_ingredient" id="filtre_ingredient" class="filtre">
-                <option value="">---Choisir un ingrédient---</option>
-                <option value="Entrée">Entrée</option>
-                <option value="Plat principal">Plat principal</option>
-                <option value="Dessert">Dessert</option>
-                <option value="Salade">Salade</option>
-            </select>        
-        </div>
-        <div role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-            <button>Fermer</button>
-            <h1 id="modal-heading">Filtre nom</h1>
-            <input name="filtre_ingredient" id="filtre_ingredient" class="filtre"></input>
-            <button id="button_ajouter_ingredient">Ajouter</button>   
-        </div>
-
-
+        <button id="button_filtre_nom">Nom</button> 
+        <button id="button_filtre_categorie">Categorie</button> 
+        <button id="button_filtre_ingredient">Ingredients</button>   
     </section>
     <section id="resultats">
 

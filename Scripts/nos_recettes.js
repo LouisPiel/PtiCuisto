87d58ee5777aa46af88d41fbceb7eba3c06b9f3a
Sirmetana <lukas.siopathis@gmail.com>
2023-10-23 18:00:08 +0200

    Ne pas continuer en JS ! Refaire la meme en PHP

--- Scripts/nos_recettes.js
+++ Scripts/nos_recettes.js
@@ -1,38 +1,96 @@
+/*TO DO : Refaire tout ça pour que le site puisse THEORIQUEMENT être une seule page qui fonctionne juste avec du PHP
+(ou un minimum de JS) et selon Modèle Vue Contrôleur*/
+/*Modales peuvent être utilisées de partout sur le site*/
+
+
+//Modales
 var modaleNom = document.getElementById("modaleNom");
-var modaleCategorie = document.getElementById("modaleCategorie");
-var modaleIngredient = document.getElementById("modaleIngredient");
+var modaleCateg = document.getElementById("modaleCategorie");
+var modaleIngr = document.getElementById("modaleIngredient");
 
+//Boutons de filtres
+var btFiltreNom = document.getElementById("button_filtre_nom");
+var btFiltreCateg = document.getElementById("button_filtre_categorie");
+var btFiltreIngr = document.getElementById("button_filtre_ingredient");
 
-var addNom = document.getElementById("button_ajouter_nom");
-var addIngr = document.getElementById("button_ajouter_ingredient");
+//Boutons d'ajout de filtre
+var btAjNom = document.getElementById("button_ajouter_nom");
+var btAjCateg = document.getElementById("button_ajouter_categorie");
+var btAjIngr = document.getElementById("button_ajouter_ingredient");
 
+//Contenu des inputs et select des filtres
 var filtreNom = document.getElementById("filtre_nom");
-var filtreCategorie = document.getElementById("filtre_categorie");
-var filtreIngredient = document.getElementById("filtre_ingredient");
+var filtreCateg = document.getElementById("filtre_categorie");
+var filtreIngr = document.getElementById("filtre_ingredient");
 
 
-// Get the <span> element that closes the modal
+// Croix (span) de fermeture des modales
 var span1 = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
 var span3 = document.getElementsByClassName("close")[2];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+// Ouvre les modales au clic 
+btFiltreNom.onclick = function () {
+    modaleNom.style.display = "block";
+    console.log("modale ouverte");
+}
+btFiltreCateg.onclick = function () {
+    modaleCateg.style.display = "block";
+    console.log("modale ouverte");
+}
+btFiltreIngr.onclick = function () {
+    modaleIngr.style.display = "block";
+    console.log("modale ouverte");
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+// Ferme la modale au clic sur la croix
+span1.onclick = function () {
+    modaleNom.style.display = "none";
+    console.log("modale fermée X");
+}
+span2.onclick = function () {
+    modaleCateg.style.display = "none";
+    console.log("modale fermée X");
+}
+span3.onclick = function () {
+    modaleIngr.style.display = "none";
+    console.log("modale fermée X");
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Ferme la modale si on clique en dehors de la feenêtre
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    switch(event.target) {
+        case modaleNom: 
+            modaleNom.style.display = "none";
+            console.log("modale fermée");
+        break;
+        case modaleCateg: 
+            modaleCateg.style.display = "none";
+            console.log("modale fermée");
+        break;
+        case modaleIngr: 
+            modaleIngr.style.display = "none";
+            console.log("modale fermée");
+        break;
     }
 }
 
-addNom.addEventListener('click', function () {
-    let inputNom = filtreNom.textContent;
+btAjNom.addEventListener('click', function () {
+    let inputNom = filtreNom.value;
+    console.log("Filtre nom : " + inputNom);
+    filtreNom.textContent="";
+    modaleNom.style.display = "none";
+});
+
+btAjCateg.addEventListener('click', function () {
+    let selectCateg = filtreCateg.value;
+    console.log("Filtre catégorie : " + selectCateg);
+    modaleCateg.style.display = "none";
+});
+
+btAjIngr.addEventListener('click', function () {
+    let inputIngr = filtreIngr.value;
+    console.log("Filtre ingrédient : " + inputIngr);
+    filtreIngr.textContent="";
+    modaleIngr.style.display = "none";
 });
--- nos_recettes.php
+++ nos_recettes.php
@@ -4,57 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos recettes</title>
-    <script src="../Scripts/nos_recettes.js"></script>
-    <style>
-        .modal {
-        display: none; /* Hidden by default */
-        position: fixed; /* Stay in place */
-        z-index: 1; /* Sit on top */
-        padding-top: 100px; /* Location of the box */
-        left: 0;
-        top: 0;
-        width: 100%; /* Full width */
-        height: 100%; /* Full height */
-        overflow: auto; /* Enable scroll if needed */
-        background-color: rgb(0,0,0); /* Fallback color */
-        background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-        }
-    </style>
+    <link rel="stylesheet" href="./Style/style.css">
 </head>
 <body>
     <!--Il faut que chaque filtre ajoute un tag qui limite la recherche-->
     <!--Fenêtres modales-->
-    <div id="modaleNom" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-        <span class="close">&times;</span>
-        <h1 id="modal-heading">Filtre nom</h1>
-        <input name="filtre_nom" id="filtre_nom" class="filtre"></input>
-        <button id="button_ajouter_nom">Ajouter</button>            
+    <div id="modaleNom" class="modal" aria-modal="true" aria-labelledby="modal-heading">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h1 id="modal-heading">Filtre nom</h1>
+            <input name="filtre_nom" id="filtre_nom" class="filtre"></input>
+            <button id="button_ajouter_nom">Ajouter</button>            
+        </div>            
     </div>
-    <div id="modaleCategorie" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-        <span class="close">&times;</span>
-        <h1 id="modal-heading">Filtre ingredient</h1>
-        <select name="filtre_ingredient" id="filtre_ingredient" class="filtre">
-            <option value="">---Choisir un ingrédient---</option>
-            <option value="Entrée">Entrée</option>
-            <option value="Plat principal">Plat principal</option>
-            <option value="Dessert">Dessert</option>
-            <option value="Salade">Salade</option>
-        </select>        
+    <div id="modaleCategorie" class="modal" aria-modal="true" aria-labelledby="modal-heading">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h1 id="modal-heading">Filtre ingredient</h1>
+            <select name="filtre_categorie" id="filtre_categorie" class="filtre">
+                <option value="">---Choisir un ingrédient---</option>
+                <option value="Entrée">Entrée</option>
+                <option value="Plat principal">Plat principal</option>
+                <option value="Dessert">Dessert</option>
+            </select>
+            <button id="button_ajouter_categorie">Ajouter</button>
+        </div>        
     </div>
-    <div id="modaleIngredient" class="modal" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-        <span class="close">&times;</span>
-        <h1 id="modal-heading">Filtre nom</h1>
-        <input name="filtre_ingredient" id="filtre_ingredient" class="filtre"></input>
-        <button id="button_ajouter_ingredient">Ajouter</button>   
+    <div id="modaleIngredient" class="modal" aria-modal="true" aria-labelledby="modal-heading">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h1 id="modal-heading">Filtre Ingredient</h1>
+            <input name="filtre_ingredient" id="filtre_ingredient" class="filtre"></input>
+            <button id="button_ajouter_ingredient">Ajouter</button>
+        </div>    
     </div>
     <section id="bandeau_filtres">
         <p>Filtres</p>
-        <button id="button_filtre_nom">Nom</button> 
-        <button id="button_filtre_categorie">Categorie</button> 
-        <button id="button_filtre_ingredient">Ingredients</button>   
+        <button id="button_filtre_nom" class="bt_filtre">Nom</button> 
+        <button id="button_filtre_categorie" class="bt_filtre">Categorie</button> 
+        <button id="button_filtre_ingredient" class="bt_filtre">Ingredients</button>   
     </section>
     <section id="resultats">
 
     </section>
+    <script src="./Scripts/nos_recettes.js"></script>
 </body>
 </html>

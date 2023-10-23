fa6d377acfd2f7ee6e1a4a55d02f26d8fe0fdd60
guideau jean <guideau221@unicaen.fr>
2023-10-23 09:54:44 +0200

    add: header avec bootstrap, manque encore quelque modification à faire

--- PHP/header.php
+++ PHP/header.php
@@ -1,27 +1,33 @@
 <?php
 echo '<header>';
 
-echo '   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+echo '  <nav class="navbar navbar-expand-lg ">
             <a class="navbar-brand" href="#"> 
-                <img src="../image-logo/Logo.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+                <img src="../image-logo/Logo.png" height="80em" class="d-inline-block align-top" alt="" />
             </a>
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+            <div class="collapse navbar-collapse d-flex justify-content-end topnav" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a href="index.php" class="nav-link">Accueil</a></li>
                     <li class="nav-item"><a href="nosRecettes.php" class="nav-link">Nos recettes</a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
+                            Filtres
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        </div>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Catégories</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Titre</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Ingrédients</a></li>
+                        </ul>
                     </li>
                     <li class="nav-item"><a href="connexion.php" class="nav-link">Connexion</a></li>
                 </ul>
-            </div>
-         </nav>';
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <span class="fa-solid fa-bars fa-2xl"></span>
+                </a>
+            </div>                
+        </nav>';
 
 echo '</header>';
 ?>

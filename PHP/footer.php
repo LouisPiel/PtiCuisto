2735f8f3e008a6a6fd5c278a17e6f18dd0bdbd35
guideau jean <guideau221@unicaen.fr>
2023-11-06 11:35:40 +0100

    add footer; refactor header;

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,7 +1,11 @@
 <?php
 echo '<footer>';
 
-echo 'test';
+echo '<div id="foot">
+            <a href="https://facebook.com"><span class="fa-brands fa-square-facebook fa-2xl icon-foot" style="color: #004080;" ></span></a>
+            <a href="https://x.com"><span class="fa-brands fa-square-x-twitter fa-2xl icon-foot"></span></a>
+            <a href="https://linkedin.com"><span class="fa-brands fa-linkedin fa-2xl icon-foot" style="color: #008080;"></span></a>
+      </div>';
 
 echo '</footer>';
 

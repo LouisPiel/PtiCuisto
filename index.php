422b94406187cdcab6ae3428ba32700e0e5ebcaf
Sirmetana <lukas.siopathis@gmail.com>
2023-11-07 11:21:27 +0100

    aled

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php 
-    //Récupération des infos de connexion dans fichier .env
-    $db_host= getenv('host');
-    $db_port= getenv('port');
-    $db_name= getenv('dbname');
-    $db_username= getenv('username');
-    $db_password= getenv('password');
 
-    require('templates/homepage.php');
+    require('templates/nos_recettes.php');
 ?>
